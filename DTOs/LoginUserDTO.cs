14b26726adcfb058bd866a1ef1d988c430f49b35
moonlaughs <iza8848@gmail.com>
2021-04-30 10:05:27 +0200

    No vlidations in loginDTO except that the fields are required

--- DTOs/LoginUserDTO.cs
+++ DTOs/LoginUserDTO.cs
@@ -8,8 +8,6 @@ namespace ToDoAPI.DTOs
         public string Login { get; init; }
 
         [Required]
-        [StringLength(100, MinimumLength = 6)]
-        [RegularExpression(@"^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$", ErrorMessage = "The password should contain at least one letter and one number!")]
         public string Password { get; init; }
     }
 }
