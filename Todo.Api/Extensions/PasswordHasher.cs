ea3f139531397fd000ffef67e6d5a87572d0a757
moonlaughs <iza8848@gmail.com>
2021-06-06 12:06:05 +0200

    Refactored a password hashing method, to store a random salt with the password

--- Todo.Api/Extensions/PasswordHasher.cs
+++ Todo.Api/Extensions/PasswordHasher.cs
@@ -1,26 +1,45 @@
 using System;
+using System.Security.Cryptography;
 using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 
 namespace Todo.Api.Extensions
 {
     public class PasswordHasher
     {
-        byte[] salt = new byte[128 / 8];
-        
+         private static byte[] salt = new byte[]{};
+        private static int saltLengthLimit = 30;
+        private static byte[] GetSalt(int maximumSaltLength)
+        {
+            salt = new byte[maximumSaltLength];
+            using (var random = new RNGCryptoServiceProvider())
+            {
+                random.GetNonZeroBytes(salt);
+            }
+            return salt;
+        }
+
         public string hashPass(string password)
         {
+            if(salt.Length == 0){
+                salt = GetSalt(saltLengthLimit);
+            }
+            
             string hashedPassword = Convert.ToBase64String(KeyDerivation.Pbkdf2(
                 password: password,
                 salt: salt,
-                prf: KeyDerivationPrf.HMACSHA1,
+                prf: KeyDerivationPrf.HMACSHA256,
                 iterationCount: 1000,
-                numBytesRequested: 256 / 8
+                numBytesRequested: 45
             ));
-            return hashedPassword;
+            var myHash = hashedPassword + Convert.ToBase64String(salt);
+            salt = new byte[]{};
+            return myHash;
         }
-        
-        public bool VerifyPassword(string storedHash, string enteredPassword )
+
+        public bool VerifyPassword(string storedHash, string enteredPassword)
         {
+            var saltFromHash = Convert.FromBase64String(storedHash.Substring(60));
+            salt = saltFromHash;
             var hashOfEntered = hashPass(enteredPassword);
             return hashOfEntered == storedHash;
         }
